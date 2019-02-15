b5e5a3ce298c52cbb872e3de5603a24e572fb9fd
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-15 22:11:42 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  for i in 0...dwarves.size
+    puts "/#{i + 1}. #{dwarves[i]}/"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteers)
+  planeteers.map{|element| element.capitalize << "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any?{|item| item.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  index = ingredients.index{|item| cheese_types.include?(item)}
+  if index
+    ingredients[index]
+  else
+    NIL
+  end
 end
